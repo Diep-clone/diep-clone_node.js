ce150caf36d414cd8192355cfacd5dc6f12c099b
ybs1164 <ybs1164@naver.com>
2019-12-16 21:27:16 +0900

    Update core.js

--- core.js
+++ core.js
@@ -2,32 +2,39 @@ var object_list = [];
 var tanktype = 0;
 
 window.onmousedown = function(e){
+  switch (e.button){
+    case 1:
+      var tank = new tanklist[tanktype];
 
-  var tank = new tanklist[tanktype];
+      object_list.push(tank);
+      tank.x=e.clientX / camera.z * window.devicePixelRatio;
+      tank.y=e.clientY / camera.z * window.devicePixelRatio;
+      tank.id = object_list.length-1;
+      tank.radius = 20; // 13 : 1렙 크기, 20 : 45렙 크기
+      tank.rotate = -1;
+      tank.opacity = 1;
 
-  object_list.push(tank);
-  tank.x=e.clientX / camera.z * window.devicePixelRatio;
-  tank.y=e.clientY / camera.z * window.devicePixelRatio;
-  tank.id = object_list.length-1;
-  tank.radius = 20; // 13 : 1렙 크기, 20 : 45렙 크기
-  tank.rotate = -1;
-  tank.opacity = 1;
-
-  im_count++;
+      im_count++;
+    break;
+    case 2:
+    break;
+    defualt:
+    break;
+  }
 }
 
 window.onkeydown = function(e){
   switch (e.keyCode){
     case 32: // Space키
-     for (var i=0;i<object_list.length;i++){
-      if (object_list[i]) object_list[i].keydown();
-     }
+      for (var i=0;i<object_list.length;i++){
+        if (object_list[i]) object_list[i].keydown();
+      }
     break;
     case 79: // O키
-    for (var i=0;i<object_list.length;i++){
-      if (object_list[i]) object_list[i].isDead = true;
-    }
-    im_count = 0;
+      for (var i=0;i<object_list.length;i++){
+        if (object_list[i]) object_list[i].isDead = true;
+      }
+      im_count = 0;
     break;
     case 220: // \키
       tanktype=(tanktype+1)%tanklist.length;
