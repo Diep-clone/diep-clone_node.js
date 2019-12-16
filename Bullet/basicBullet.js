6b85f1d4c6231fa7d4116a1805d8c346e918a6f8
ybs1164 <ybs1164@naver.com>
2019-12-16 19:21:26 +0900

    Add files via upload

---
+++ Bullet/basicBullet.js
@@ -0,0 +1,9 @@
+function BasicBullet(tank,sx,sy){
+  this.color = new RGB(0,176,225);
+  this.owner = tank;
+  this.speedX = sx;
+  this.speedY = sy;
+  this.time = 3000;
+}
+BasicBullet.prototype = new DynamicObject();
+BasicBullet.prototype.constructor = BasicBullet;
