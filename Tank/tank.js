36f2b293620223bd9ba1c027d3b5b95e47d7d3dd
ybs1164 <ybs1164@naver.com>
2019-12-16 19:23:08 +0900

    Update tank.js

--- Tank/tank.js
+++ Tank/tank.js
@@ -225,7 +225,7 @@ TriAngle.prototype.constructor = TriAngle;
 
 function Destroyer(){
   this.clearGun();
-  this.addGun(new Gun(this,[[0,0],[0.4,0],[0.8,1.9],[-0.8,1.9],[-0.8, 0]],0));
+  this.addGun(new Gun(this,[[0,0],[0.7,0],[0.7,1.9],[-0.7,1.9],[-0.7, 0]],0));
   this.tankType = "Destroyer";
 }
 Destroyer.prototype = new Tank();
