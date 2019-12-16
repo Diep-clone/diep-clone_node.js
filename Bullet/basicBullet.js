826229f54c361200924dd5e115067fad53d2f3bd
ybs1164 <ybs1164@naver.com>
2019-12-16 19:45:02 +0900

    Update basicBullet.js

--- Bullet/basicBullet.js
+++ Bullet/basicBullet.js
@@ -1,10 +1,21 @@
 function BasicBullet(tank,sx,sy){
   this.color = new RGB(0,176,225);
   this.owner = tank;
-  this.rotate = 0;
   this.speedX = sx;
   this.speedY = sy;
   this.time = 3000;
+  this.canvas = document.createElement("canvas");
+  this.ctx = canvas.getContext("2d");
+  this.animate = function(){
+    this.move();
+  }
+  this.move = function(){
+    this.addForce(this.speedX,this.speedY);
+  }
+  this.draw = function(){
+    
+  }
 }
 BasicBullet.prototype = new DynamicObject();
 BasicBullet.prototype.constructor = BasicBullet;
+
