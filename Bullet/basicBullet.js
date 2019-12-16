7089725b1f30ab44672547a90498d3c55bec6c28
ybs1164 <ybs1164@naver.com>
2019-12-16 19:47:45 +0900

    Update basicBullet.js

--- Bullet/basicBullet.js
+++ Bullet/basicBullet.js
@@ -6,14 +6,52 @@ function BasicBullet(tank,sx,sy){
   this.time = 3000;
   this.canvas = document.createElement("canvas");
   this.ctx = canvas.getContext("2d");
+  this.canvasPos = {x:0,y:0};
   this.animate = function(){
     this.move();
   }
   this.move = function(){
     this.addForce(this.speedX,this.speedY);
   }
+  this.setCanvasSize = function(){
+    this.canvas.width = (this.radius * 2) * camera.z;
+    this.canvas.height = (this.radius * 2) * camera.z;
+    this.canvasPos = {x:this.radius * camera.z,y:this.radius * camera.z};
+    this.canvas.width += 4 * camera.z + 6;
+    this.canvas.height += 4 * camera.z + 6;
+    this.canvasPos.x += 2 * camera.z + 2;
+    this.canvasPos.y += 2 * camera.z + 2;
+    this.ctx.lineWidth = 2 * camera.z;
+    this.ctx.lineCap = "round";
+    this.ctx.lineJoin = "round";
+  }
   this.draw = function(){
-    
+    this.setCanvasSize();
+
+    this.ctx.strokeStyle = this.color.getDarkRGB(); // 몸체 그리기
+    this.ctx.fillStyle = this.color.getRGB();
+    this.ctx.beginPath();
+    this.ctx.arc(this.canvasPos.x,this.canvasPos.y,this.radius * camera.z,0,Math.PI * 2);
+    this.ctx.fill();
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = "#777777";
+    this.ctx.beginPath();
+    this.ctx.moveTo(0,0);
+    this.ctx.lineTo(this.canvas.width,this.canvas.height);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.beginPath();
+    this.ctx.moveTo(0,this.canvas.height);
+    this.ctx.lineTo(this.canvas.width,0);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.strokeRect(0,0,this.canvas.width,this.canvas.height);
+
+    ctx.globalAlpha = this.opacity;
+    ctx.drawImage(this.canvas,this.x * camera.z-this.canvasPos.x,this.y * camera.z-this.canvasPos.y);
   }
 }
 BasicBullet.prototype = new DynamicObject();
