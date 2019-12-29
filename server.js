c45ba675b29fb93360e71f2990b857706b3813ae
ybs1164 <ybs1164@naver.com>
2019-12-29 11:23:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ let sockets = {};
 
 let mapSize = {x: 0,y: 0};
 
-app.get('/', (req, res) => {
-return res.render('./index.html');
-})
+app.use((req, res) => {res.sendFile('./index.html', {root: __dirname})});
 
 function randomRange (x,y){
   if (x>y){
