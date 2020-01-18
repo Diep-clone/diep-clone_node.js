4a82a8656467103fddc242f13159e89c6b7bafd2
ybs1164 <ybs1164@naver.com>
2020-01-18 22:02:14 +0900

    Modify Speed value

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ io.on('connection', (socket) => { // 접속.
         guns:[],
         stats:[0,0,0,0,0,0,0,0],
         bulletCount:0,
-        type:12,
+        type:0,
         isCollision:false,
         hitTime:Date.now(),
         isDead:false
