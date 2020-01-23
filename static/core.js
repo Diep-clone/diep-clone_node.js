289522b7c9abbbf3bcef0d080d9ab692100c6b1f
ybs1164 <ybs1164@naver.com>
2020-01-23 23:25:25 +0900

    Fix Turn

--- static/core.js
+++ static/core.js
@@ -181,9 +181,9 @@ function System(name){ // 게임의 전체 진행 담당
             objTank.setLevel(tankList[key].level);
             this.drawObject.setSight(tankList[key].sight);
           }
-          if (!tankList[key].isCanDir){
+          //if (!tankList[key].isCanDir){
             objTank.setRotate(tankList[key].rotate);
-          }
+          //}
         }
         else{
           let objTank = this.createTankObject(tankList[key].id,this.tankList[tankList[key].type]);
