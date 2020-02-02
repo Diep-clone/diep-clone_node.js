86e36cb353d20edce37786f4ada08a975192c5a2
[JW] CJaewon <32125218+cjaewon@users.noreply.github.com>
2020-02-02 10:11:21 +0900

    edit css

--- static/run.js
+++ static/run.js
@@ -2,10 +2,10 @@ var socket = io.connect({transports: ['websocket']});
 
 let system;
 function start() {
-    console.log()
     if (event.keyCode == 13) {
         document.getElementById('textInput').style.display = 'none';
         document.getElementById('canvas').style.display = 'block';
+        
         system = new System(document.getElementById('textInput').value);
         system.drawObject.resize();
     }
