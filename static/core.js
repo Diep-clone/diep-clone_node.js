81fa82a5d686ca7936a0d7b490f05e4b3c7b277b
ybs1164 <ybs1164@gmail.com>
2020-03-03 15:17:21 +0900

    Error Fix

--- static/core.js
+++ static/core.js
@@ -221,10 +221,10 @@ function System(name){ // 게임의 전체 진행 담당
   }.bind(this));
 
   socket.on('spawn',(data) => {
-    this.isGameStart = true;
     this.controlTank = this.createObject(data.id,this.tankList[data.type]);
     this.controlTank.setPosition(data.x,data.y);
     this.controlTank.setName(data.name);
+    this.isGameStart = true;
   });
 
   socket.on('playerSet',(data)=>{
